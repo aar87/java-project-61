3827734aa7852f75352d1dca6cd517fd56f61d37
qaar <anton.raevsky.dev@gmail.com>
2024-09-28 20:10:14 +0300

    fix rule typo

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -15,7 +15,7 @@ public class App {
             new Game("Exit", "", null, false, false),
             new Game("Greet", "", null, true),
             new Game("Even", "Answer 'yes' if the number is even, otherwise answer 'no'.", new Even()),
-            new Game("Calc", "What is the result of expression?'.", new Calc()),
+            new Game("Calc", "What is the result of the expression?'.", new Calc()),
             new Game("Gcd", "Find the greatest common divisor of given numbers.", new Gcd()),
             new Game("Progression", "What number is missing in this progression?", new Progression()),
             new Game("Prime", "Answer 'yes' if given number is prime. Otherwise answer 'no'.", new Prime()),
