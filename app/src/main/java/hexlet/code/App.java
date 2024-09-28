5ace0744eba803aa1cb2b858af7b0cdec8716785
qaar <anton.raevsky.dev@gmail.com>
2024-09-28 20:14:34 +0300

    fix progression typo

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -17,7 +17,7 @@ public class App {
             new Game("Even", "Answer 'yes' if the number is even, otherwise answer 'no'.", new Even()),
             new Game("Calc", "What is the result of the expression?'.", new Calc()),
             new Game("Gcd", "Find the greatest common divisor of given numbers.", new Gcd()),
-            new Game("Progression", "What number is missing in this progression?", new Progression()),
+            new Game("Progression", "What number is missing in the progression??", new Progression()),
             new Game("Prime", "Answer 'yes' if given number is prime. Otherwise answer 'no'.", new Prime()),
     };
 
